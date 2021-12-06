0313a0f60fd7d3b88c8ef1fb9025c6503335f9db
kara-marg <71691933+kara-marg@users.noreply.github.com>
2021-12-06 02:58:34 +0200

    Add files via upload

---
+++ OOP/lab1/lab1.2/Header.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+
+
+
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+void out_BLUE_RED(const char str[100])
+{
+	SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_RED);
+	std::cout << str << std::endl;
+	SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+}
+void out_RED(const char str[100])
+{
+	SetConsoleTextAttribute(console, FOREGROUND_RED);
+	std::cout << str << std::endl;
+	SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+}
+void out_GREEN(const char str[100])
+{
+	SetConsoleTextAttribute(console, FOREGROUND_GREEN);
+	std::cout << str << std::endl;
+	SetConsoleTextAttribute(console, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+}
